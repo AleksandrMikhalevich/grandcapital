10c84d66aac51a6c1fb1cf5be13a9dea91c70304
AleksandrMikhalevich <aleksandr.mikhalevich89@gmail.com>
2025-04-25 18:02:05 +0300

    Create datasource and entities

---
+++ src/main/java/by/mikhalevich/grandcapitaltesttask/GrandCapitalTestTaskApplication.java
@@ -0,0 +1,13 @@
+package by.mikhalevich.grandcapitaltesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandCapitalTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrandCapitalTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/mikhalevich/grandcapitaltesttask/GrandCapitalTestTaskApplicationTests.java
@@ -0,0 +1,15 @@
+package by.mikhalevich.grandcapitaltesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class GrandCapitalTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
